4b1904c43aa31811609db57efbf796e92550ab11
vlad <alekseev@spiks.ru>
2024-01-23 19:04:54 +0300

    add assertion

--- test_items.py
+++ test_items.py
@@ -1,3 +1,11 @@
+from selenium.webdriver.common.by import By
+import time
+
+
 def test_button(browser):
     browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
+    time.sleep(15)
+    browser.implicitly_wait(5)
+    button = browser.find_element(By.CSS_SELECTOR, "#add_to_basket_form button[type='submit']")
+    assert button is not None
 
