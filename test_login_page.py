68660f18feea5750149a0496f76f559c47128a61
vlad <alekseev@spiks.ru>
2024-02-02 15:01:13 +0300

    rework get login page for test

--- test_login_page.py
+++ test_login_page.py
@@ -1,9 +1,12 @@
 from selenium.webdriver.common.by import By
 from .pages.login_page import LoginPage
+from .pages.main_page import MainPage
 
 
 def test_guest_can_go_to_login_page(browser):
-    link = "https://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
-    page = LoginPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()                      # открываем страницу
-    page.should_be_login_page()      # проверим что действительно страница логина
+    link = "https://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()
+    page.go_to_login_page()          # открываем страницу логина по кнопке
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
